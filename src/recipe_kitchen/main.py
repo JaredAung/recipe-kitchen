86d35abcc498d601b061dca0082486e2c917fef6
JaredAung <133064427+JaredAung@users.noreply.github.com>
2026-09-05 17:28:37 -0700

    feat:CORS

--- src/recipe_kitchen/main.py
+++ src/recipe_kitchen/main.py
@@ -1,10 +1,19 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from recipe_kitchen.api.routes import api_router
+from recipe_kitchen.core.config import get_settings
 
 logging.getLogger("recipe_kitchen").setLevel(logging.INFO)
 
 app = FastAPI(title="recipe-kitchen")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=get_settings().cors_origin_list,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router)
